cf17658dbccfe778e588edbc9f08975cdabcbe84
lucastgl <lucastgarcialauman@gmail.com>
2023-04-10 17:23:52 -0300

    Se agrega la funcionalidad de favoritos, falta evitar duplicados

--- src/Screen/Favorites.jsx
+++ src/Screen/Favorites.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, {useContext} from 'react'
+
+import {Link} from "react-router-dom"
+import dentista from "../imgs/dentista.jpg"
+import { StateContext } from '../Context/StateContext'
+import { Box, IconButton, Tooltip } from "@mui/material"
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { HomeContainer, DentistaProfile, DentistaPic } from '../Styles/styledComponent';
+
 
 const Favorites = () => {
+
+  const {state} = useContext(StateContext);
+
   return (
-    <div>Favs</div>
+    <main>
+      <h1>Favoritos</h1>
+      <HomeContainer>
+        {
+          state.data.map((item)=>(
+            <DentistaProfile key={item.id}>
+                <p>{item.name}</p>
+                <DentistaPic src={dentista}/>
+                <Link to={`/users/${item.login}`}>Ver mas</Link>
+                <Box>
+                <Tooltip title="agregar a favoritos">
+                    <IconButton>
+                        <FavoriteBorderIcon/>
+                    </IconButton>
+                </Tooltip>
+                </Box>
+            </DentistaProfile>
+        ))}
+      </HomeContainer>
+    </main>
   )
 }
 
--- src/Screen/Home.jsx
+++ src/Screen/Home.jsx
@@ -1,14 +1,21 @@
-import React, {useState, useEffect} from 'react'
-import { HomeContainer, DentistaProfile, DentistaPic } from '../Styles/styledComponent';
-import dentista from "../imgs/dentista.jpg"
+import React, {useState, useEffect, useContext} from 'react'
+
 import { Link } from 'react-router-dom';
+import { StateContext } from '../Context/StateContext';
+import { Box, IconButton, Tooltip } from '@mui/material';
+import { HomeContainer, DentistaProfile, DentistaPic } from '../Styles/styledComponent';
+
 import axios from "axios";
+import dentista from "../imgs/dentista.jpg"
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 export const endpoint = "https://jsonplaceholder.typicode.com/users";
 
 const Home = () => {
 
   const [values, setValues] = useState(null);
+  const { dispatch } = useContext(StateContext);
+
 
   useEffect(()=>{
     axios.get(endpoint)
@@ -29,6 +36,13 @@ const Home = () => {
             <p>{item.name}</p>
             <DentistaPic src={dentista} alt="dentista"/>
             <Link to={`/detail/${item.name}`}>Ver mas</Link>
+            <Box>
+              <Tooltip title="agregar a favoritos">
+                <IconButton onClick={()=> dispatch({type: "FAVS", payload: item})}>
+                  <FavoriteBorderIcon/>
+                </IconButton>
+              </Tooltip>
+            </Box>
           </DentistaProfile>
       ))}
     </HomeContainer>
